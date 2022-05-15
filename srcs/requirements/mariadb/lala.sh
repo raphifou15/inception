032fa1b93bb1bb7748e8860fe65fd13dc498d7eb
rkhelif <rkhelif@student.42.fr>
2022-05-16 00:06:32 +0200

    commit de raph

---
+++ srcs/requirements/mariadb/lala.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -xv
+#creation de la base de donner
+service mysql start
+echo "CREATE DATABASE IF NOT EXISTS WordPress;" | mysql -u root --skip-password
+#creation d'un nouvel utilisateur qui peut utiliser la base de donner
+echo "CREATE USER IF NOT EXISTS 'rkhelif' IDENTIFIED BY 'rkhelif';" | mysql -u root --skip-password
+service mysql stop
+exec mysqld_safe
