15d30cee0469989597432b4ff130d4fb71cb21ba
rkhelif <rkhelif@student.42.fr>
2022-05-18 18:34:04 +0200

    connextion php effectuer

--- srcs/requirements/mariadb/lala.sh
+++ srcs/requirements/mariadb/lala.sh
@@ -2,8 +2,10 @@
 set -xv
 #creation de la base de donner
 service mysql start
-echo "CREATE DATABASE IF NOT EXISTS WordPress;" | mysql -u root --skip-password
+echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
 #creation d'un nouvel utilisateur qui peut utiliser la base de donner
 echo "CREATE USER IF NOT EXISTS 'rkhelif' IDENTIFIED BY 'rkhelif';" | mysql -u root --skip-password
+echo "GRANT ALL ON wordpress.* TO 'rkhelif' IDENTIFIED BY 'rkhelif';" | mysql -u root --skip-password 
+
 service mysql stop
 exec mysqld_safe
