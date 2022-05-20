2825242e0eb84df853da0c26cc1c4d68d11d0b78
rkhelif <rkhelif@student.42.fr>
2022-05-20 16:14:45 +0200

    raph

--- srcs/requirements/wordpress/lala2.sh
+++ srcs/requirements/wordpress/lala2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xv
-mv wordpress /var/www/html/
-
+mv wordpress/* /var/www/html/wordpress/
+chown -R www-data /var/www/html
+mv wp-config.php /var/www/html/wordpress
 exec php-fpm7.3 -F
