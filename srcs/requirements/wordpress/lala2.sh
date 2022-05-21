802532b932e8c4b969e876668630f7751f8d64d4
rkhelif <rkhelif@student.42.fr>
2022-05-21 19:54:19 +0200

    il faut faire les variables d'environement et le makefile

--- srcs/requirements/wordpress/lala2.sh
+++ srcs/requirements/wordpress/lala2.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -xv
-mv wordpress/* /var/www/html/wordpress/
+#mv wordpress/* /var/www/html/wordpress/
 chown -R www-data /var/www/html
-mv wp-config.php /var/www/html/wordpress
+#mv wp-config.php /var/www/html/
+cd /var/www/html/
+sleep 10
+wp config create --dbname=wordpress --dbuser=rkhelif --dbpass=rkhelif --dbhost=mariadb  --allow-root
+wp core install --url=rkhelif.42.fr --title=rkhelif --admin_user=rkhelif --admin_password=rkhelif --admin_email=lala@lala.fr --allow-root
+wp user create lala lala2@lala.fr --role=editor --user_pass=lala --allow-root
 exec php-fpm7.3 -F
