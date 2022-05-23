0c7117546dc0c31e0c69cfc350bb26a8c21fa0e1
rkhelif <rkhelif@student.42.fr>
2022-05-23 18:11:24 +0200

    raphifou

--- srcs/requirements/mariadb/lala.sh
+++ srcs/requirements/mariadb/lala.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
+export DB_ROOT_PASS=raph
+
+export DB_NAME=wordpress
+export DB_USER=rkhelif
+export DB_PASS=rkhelif
+
 set -xv
 #creation de la base de donner
 service mysql start
-echo "CREATE DATABASE IF NOT EXISTS wordpress;" | mysql -u root --skip-password
-#creation d'un nouvel utilisateur qui peut utiliser la base de donner
-echo "CREATE USER IF NOT EXISTS 'rkhelif' IDENTIFIED BY 'rkhelif';" | mysql -u root --skip-password
-echo "GRANT ALL ON wordpress.* TO 'rkhelif' IDENTIFIED BY 'rkhelif';" | mysql -u root --skip-password
+#mysql -V
+envsubst < /lala.sql > /lala2.sql
+mysql -Dmysql < /lala2.sql | true
 
-service mysql stop
+#echo "CREATE DATABASE IF NOT EXISTS $DB_NAME;" | mysql -u root -p$DB_ROOT_PASS
+#creation d'un nouvel utilisateur qui peut utiliser la base de donner
+#echo "CREATE USER IF NOT EXISTS '$DB_USER' IDENTIFIED BY '$DB_PASS';" | mysql -u root -p$DB_ROOT_PASS
+#echo "GRANT ALL ON $DB_NAME.* TO '$DB_USER' IDENTIFIED BY '$DB_PASS';" | mysql -u root -p$DB_ROOT_PASS
+#echo "GRANT ALL PRIVILEGES on *.* to 'root'@'localhost' identified by '$DB_ROOT_PASS';" | mysql -u root -p$DB_ROOT_PASS
+#echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';" | mysql -u root -p$DB_ROOT_PASS
+#echo "FLUSH PRIVILEGES;" | mysql -u root -p$DB_ROOT_PASS
+# sleep 4;
+service mysql stop | echo -n ""
+
 exec mysqld_safe
--- srcs/requirements/wordpress/lala2.sh
+++ srcs/requirements/wordpress/lala2.sh
@@ -5,7 +5,7 @@ chown -R www-data /var/www/html
 #mv wp-config.php /var/www/html/
 cd /var/www/html/
 sleep 10
-wp config create --dbname=wordpress --dbuser=rkhelif --dbpass=rkhelif --dbhost=mariadb  --allow-root
-wp core install --url=rkhelif.42.fr --title=rkhelif --admin_user=rkhelif --admin_password=rkhelif --admin_email=lala@lala.fr --allow-root
-wp user create lala lala2@lala.fr --role=editor --user_pass=lala --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb  --allow-root
+wp core install --url=rkhelif.42.fr --title=$WP_TITLE --admin_user=$WP_AD_USER --admin_password=$WP_AD_PASS --admin_email=$WP_AD_EMAIL --allow-root
+wp user create $WP_U_USER $WP_U_EMAIL --role=editor --user_pass=$WP_U_PASS --allow-root
 exec php-fpm7.3 -F
